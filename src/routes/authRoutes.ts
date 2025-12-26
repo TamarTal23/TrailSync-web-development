1073e6b5d8f4f60cfddeee0e1f82ab0bfd9dbe13
TamarTal23 <212927487@idf.il>
2025-12-26 08:43:51 +0200

    fix auth route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import userController from '../controllers/userController';
-import { authenticate } from '../middlewares/authMiddleware';
+import authController from '../controllers/authController';
 
-router.get('/', userController.getAllUsers.bind(userController));
-router.get('/:id', userController.getUserById.bind(userController));
+router.post('/register', authController.register);
 
-router.put('/:id', authenticate, userController.updateUser.bind(userController));
+router.post('/login', authController.login);
 
-router.delete('/:id', authenticate, userController.deleteUser.bind(userController));
+router.post('/refresh-token', authController.refreshToken);
 
 export default router;
