8efce7c8e461a6f2eefd8557f92355dd48970afd
ofirShriki <ofir3224@rogozin.net>
2026-02-22 01:27:41 +0200

    🐞 fix(notes): removed notes

--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -45,8 +45,6 @@ export const buildFiltersFromParsedQuery = (parsedQuery: Partial<ParsedPostQuery
 };
 
 export const buildFilterQuery = (queryParams: Record<string, any>): Record<string, any> => {
-  // const filter: Record<string, any> = {};
-
   const filter = buildFiltersFromParsedQuery({
     daysRange: {
       min: queryParams.minDays,
@@ -58,35 +56,6 @@ export const buildFilterQuery = (queryParams: Record<string, any>): Record<strin
       country: queryParams.country,
     },
   });
-
-  // if (queryParams.minDays) {
-  //   const minDays = parseInt(queryParams.minDays as string);
-  //   if (!isNaN(minDays)) {
-  //     filter.numberOfDays = { ...filter.numberOfDays, $gte: minDays };
-  //   }
-  // }
-
-  // if (queryParams.maxDays) {
-  //   const maxDays = parseInt(queryParams.maxDays as string);
-  //   if (!isNaN(maxDays)) {
-  //     filter.numberOfDays = { ...filter.numberOfDays, $lte: maxDays };
-  //   }
-  // }
-
-  // if (queryParams.maxPrice) {
-  //   const maxPrice = parseInt(queryParams.maxPrice as string);
-  //   if (!isNaN(maxPrice)) {
-  //     filter.price = { $lte: maxPrice };
-  //   }
-  // }
-
-  // if (queryParams.city) {
-  //   filter['location.city'] = queryParams.city;
-  // }
-
-  // if (queryParams.country) {
-  //   filter['location.country'] = queryParams.country;
-  // }
 
   const handledParams = ['minDays', 'maxDays', 'maxPrice', 'city', 'country'];
 
