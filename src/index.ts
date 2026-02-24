6ac868d0e594c18a82aa619d9161d904ee5e5b1b
TamarTal23 <212927487@idf.il>
2026-02-24 15:03:28 +0200

    fix: update paths for SSL key and certificate in production setup

--- src/index.ts
+++ src/index.ts
@@ -52,8 +52,8 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   console.log('PRODUCTION');
   try {
-    const keyPath = path.resolve(__dirname, '../../client-key.pem');
-    const certPath = path.resolve(__dirname, '../../client-cert.pem');
+    const keyPath = path.resolve(__dirname, '../../../client-key.pem');
+    const certPath = path.resolve(__dirname, '../../../client-cert.pem');
     
     const options = {
       key: fs.readFileSync(keyPath),
